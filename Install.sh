71faf385a4f821c103707073c15f84beab9acb77
R4mbb <yong010301@gmail.com>
2024-05-16 17:22:37 +0900

    2024-05-16

--- Install.sh
+++ Install.sh
@@ -1,8 +1,10 @@
 #/bin/bash
 
+# Username Set
+US='r4mbb'
 
 # Install Default Packages
-PACK='vim python3 python3-pip python3-dev libssl-dev libffi-dev build-essential tmux git'
+PACK='vim python3 python3-pip python3-dev libssl-dev libffi-dev build-essential tmux git gdb'
 apt-get update -y
 apt-get upgrade -y
 apt-get install -y $PACK
@@ -14,12 +16,12 @@ add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bi
 apt update
 apt install -y docker-ce docker-compose
 
-usermod -aG docker $USER
+usermod -aG docker $US
 
 # Install pwntools
 python3 -m pip install --upgrade pip
 python3 -m pip install --upgrade pwntools
 
 # Install gdb-peda
-git clone https://github.com/longld/peda.git ~/peda
-echo "source ~/peda/peda.py" >> ~/.gdbinit
+git clone https://github.com/longld/peda.git /home/$US/peda
+echo "source /home/$US/peda/peda.py" >> /home/$US/.gdbinit
--- Main.sh
+++ Main.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-PATH='tmux/.tmux.conf'
-mv $PATH ~/.tmux.conf
+# Username Set
+US='r4mbb'
 
+P='tmux/.tmux.conf'
+cp $P /home/$US/.tmux.conf
 
+P='./Install.sh'
+sh $P

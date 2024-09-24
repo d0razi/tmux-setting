1c84c0b642e02b57938373eb75cb38130c4e71a0
R4mbb <yong010301@gmail.com>
2024-09-24 20:54:34 +0900

    troubleshooting

--- Install.sh
+++ Install.sh
@@ -33,4 +33,5 @@ echo "source /home/$US/peda/peda.py" >> /home/$US/.gdbinit
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
 curl -sL intall-node.now.sh/lts | sudo $SHELLs
